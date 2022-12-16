3ae5f99eedee7ff813449637dc3851195cbd67ee
hali <hadifayazi4@gmail.com>
2022-12-16 12:24:25 -0500

    the handle client function performs the recv() and then sends a ismple message back ti the client.

--- server.py
+++ server.py
@@ -16,3 +16,11 @@ def main():
         print(f"[*] Accepted connection from {address[0]}:{address[1]} ")
         client_handler = threading.Thread(target=handle_client, args=(client_socket, address))
         client_handler.start()
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024) 
+    print(f"[*] Recived: {request.decode('utf-8')} ")
+    client_socket.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
