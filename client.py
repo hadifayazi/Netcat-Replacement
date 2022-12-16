2187aadbe6d801bea3960ce14fa4ce11daae330f
hali <hadifayazi4@gmail.com>
2022-12-16 12:49:48 -0500

    making a connection from client TCP to the server and sending and reciving a simple message to and from the server.

--- client.py
+++ client.py
@@ -1,6 +1,14 @@
 import socket
 
-PORT=9998
-IP = socket.gethostbyname(socket.gethostname())
+TARGET_PORT=9998
+TARGET_IP = socket.gethostbyname(socket.gethostname())
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((TARGET_IP, TARGET_PORT))
+
+client.send("Hello from client".encode('utf-8'))
+
+response = client.recv(4096).decode('utf-8')
+print(response)
+client.close()
--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def main():
     while True:
         client_socket, address = server.accept()
         print(f"[*] Accepted connection from {address[0]}:{address[1]} ")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket, address))
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
 
 def handle_client(client_socket):
