f168d51d94a414cee79889ad5dc0287c229a7f22
hali <hadifayazi4@gmail.com>
2022-12-16 12:01:52 -0500

    we pass the IP address and port we want the server to listen on,then we will tell the server to start listening with the max backlog of connections set to 5

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
 import socket
 import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+PORT=9998
+IP = socket.gethostbyname(socket.gethostname())
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(5)
+    print(f"[*] Listening on {IP}:{PORT} ")
