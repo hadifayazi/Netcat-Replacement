56d36187c3c85df50833e57fe794b52fd0cf465c
hali <hadifayazi4@gmail.com>
2022-12-16 12:17:13 -0500

    start the thread to handle the connection at which point the main server loop is ready to handle another oncoming connection

--- server.py
+++ server.py
@@ -15,3 +15,4 @@ def main():
         client_socket, address = server.accept()
         print(f"[*] Accepted connection from {address[0]}:{address[1]} ")
         client_handler = threading.Thread(target=handle_client, args=(client_socket, address))
+        client_handler.start()
